2df9fc2ff9de825d3e4283e8d8a39c63f72407b3
yakovgavryushov <95687203+yakovgavryushov@users.noreply.github.com>
2023-02-17 07:10:02 +0300

    Repo last ver add

--- models/Repo.js
+++ models/Repo.js
@@ -5,6 +5,7 @@ const Repo = db.define("newrepo", {
   item_id: { type: Sequelize.STRING },
   item_name: { type: Sequelize.STRING },
   full_name: { type: Sequelize.STRING },
+  stargazers_count: { type: Sequelize.STRING },
   description: { type: Sequelize.STRING },
   html_url: { type: Sequelize.STRING },
 });
