d74d30e817fcf75492f33296114cee1e4cd50cb2
yakovgavryushov <95687203+yakovgavryushov@users.noreply.github.com>
2023-02-21 15:36:16 +0300

    unexpected token deleted

--- models/Repo.js
+++ models/Repo.js
@@ -6,11 +6,7 @@ const Repo = db.define("newrepo", {
   item_name: { type: Sequelize.STRING },
   full_name: { type: Sequelize.STRING },
   stargazers_count: { type: Sequelize.STRING },
-<<<<<<< HEAD
   description: { type: Sequelize.TEXT },
-=======
-  description: { type: Sequelize.STRING },
->>>>>>> master
   html_url: { type: Sequelize.STRING },
 });
 
